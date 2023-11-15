8dbd03e6830bee7ca1b44300ee54620bebf828fe
Vasil <58910106+vasetousa@users.noreply.github.com>
2023-11-15 15:23:21 +0100

    re-visiting projects

---
+++ Testing/List/test_list.py
@@ -0,0 +1,68 @@
+from unittest import TestCase as Case, main
+from Testing.List.extended_list import IntegerList
+
+
+class TestList(Case):
+
+    def setUp(self):
+        self.data_list = [1, 2, 'Peter']
+        self.il = IntegerList(*self.data_list)
+        self.index_len = len(self.il.get_data())
+
+    def test_if_init(self):
+        self.assertEqual(self.il.get_data(), [1, 2])
+
+    def test_add_element_to_list_if_not_int_raises(self):
+        # part 1
+        with self.assertRaises(ValueError) as ve:
+            self.il.add('Peter')
+        self.assertEqual(str(ve.exception), 'Element is not Integer')
+        # part 2
+        self.assertNotEqual(self.il.get_data(), [1, 2, 100])
+        self.il.add(100)
+        self.assertEqual(self.il.get_data(), [1, 2, 100])
+
+    def test_remove_index_from_list_if_out_of_range_raises(self):
+        # part 1
+        self.assertNotEqual(self.index_len, 3)
+        with self.assertRaises(IndexError) as ie:
+            self.il.remove_index(3)
+        self.assertEqual(str(ie.exception), 'Index is out of range')
+        # part 2
+        self.assertEqual(self.il.get_data(), [1, 2])
+        self.il.remove_index(1)
+        self.assertEqual(self.il.get_data(), [1])
+
+    def test_get_element_from_index_if_index_out_of_range_raises(self):
+        # part 1
+        self.assertNotEqual(self.index_len, 3)
+        with self.assertRaises(IndexError) as ie:
+            self.il.get(3)
+        self.assertEqual(str(ie.exception), 'Index is out of range')
+        # part 2
+        self.assertEqual(self.il.get_data(), [1, 2])
+        el = self.il.get(1)
+        self.assertEqual(el, 2)
+
+    def test_insert_raises(self):
+        # part 1 if element is not integer
+        with self.assertRaises(ValueError) as ve:
+            self.il.insert(0, 'Peter')
+        self.assertEqual(str(ve.exception), 'Element is not Integer')
+        # part 2 if index is out of range
+        self.assertNotEqual(self.index_len, 3)
+        with self.assertRaises(IndexError) as ie:
+            self.il.insert(3, 100)
+        self.assertEqual(str(ie.exception), 'Index is out of range')
+        # part 3 if element was insert in the list
+        self.assertEqual(self.il.get_data(), [1, 2])
+        self.il.insert(1, 100)
+        self.assertEqual(self.il.get_data(), [1, 100, 2])
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
