decfd161794ddf8a25507c770a6508cd6ddacce6
Vasil <58910106+vasetousa@users.noreply.github.com>
2023-11-20 17:23:09 +0100

    added a check fro band name

--- Exam_19_Dec_2022/project/band.py
+++ Exam_19_Dec_2022/project/band.py
@@ -4,5 +4,15 @@ class Band:
         self.members = []
         self.member_names = []
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        if value == "" or value.isspace():
+            raise ValueError("Band name should contain at least one character!")
+        self.__name = value
+
     def __str__(self):
         return f"{self.name} with {len(self.members)} members."
