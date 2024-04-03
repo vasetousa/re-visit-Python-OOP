b66b67fe27cfc2eb110b57fa80fe48c445c01877
Vasil <58910106+vasetousa@users.noreply.github.com>
2024-04-03 20:05:15 +0200

    Delete Django_re-visit_23-24 directory

--- Django_re-visit_23-24/Tasks/Tasks/base/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from Tasks.base.models import Task
-
-
-# Register your models here.
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description')
